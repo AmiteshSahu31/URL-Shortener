7cf0cb0d2766b37bb8a282a1a4f4e6ad85b87fce
AmiteshSahu31 <amiteshsahu2002@gmail.com>
2025-04-21 02:12:26 +0530

    signup UI and protected routes

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import { useSearchParams } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import Login from '@/components/login';
 import Signup from '@/components/signup';
+import { UrlState } from '@/context';
 
 
 const Auth = () => {
-  const [searchParams,setSearchParams]= useSearchParams();
+  const [searchParams]= useSearchParams();
+  const navigate= useNavigate();
+  const longLink = searchParams.get("createNew");
+
+  const {isAuthenticated,loading}=UrlState();
+
+  useEffect(() => {
+    if (isAuthenticated) navigate(`/dashboard?${longLink ? `createNew=${longLink}` : ""}`);  
+  },[isAuthenticated,loading]);
+  
   return (
-    <div className='mt-36 gap-10 flex items-center flex-col'>
+    <div className='mt-28 gap-10 flex items-center flex-col'>
      <h1 className='text-3xl font-extrabold'>
-     {searchParams.get("createNew") ? "Hold up Let's Login first" : "Login/Signup"}
+     {longLink ? "Hold up Let's Login first" : "Login/Signup"}
      </h1>
   <Tabs defaultValue="login" className="w-[400px] rounded-md">
   <TabsList className={"w-full grid grid-cols-2"}>
